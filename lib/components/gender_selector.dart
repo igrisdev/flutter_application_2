246bf1cffd493f21991beafd3e6b5bf8bd861bab
igrisdev <johanalvarezz237@gmail.com>
2025-06-10 16:22:40 -0500

    Implement gender selector with visual feedback and add height selector component

--- lib/components/gender_selector.dart
+++ lib/components/gender_selector.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/core/app_colors.dart';
+import 'package:flutter_application_2/core/text_styles.dart';
 
 class GenderSelector extends StatefulWidget {
   const GenderSelector({super.key});
@@ -14,8 +16,74 @@ class _GenderSelectorState extends State<GenderSelector> {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Column(children: []),
-        Column(children: []),
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedGender = "Hombre";
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 16,
+                top: 16,
+                bottom: 16,
+                right: 8,
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: selectedGender == "Hombre"
+                      ? AppColors.backgroundComponentSelected
+                      : AppColors.backgroundComponent,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      Image.asset("assets/images/male.png", height: 100),
+                      Text("Hombre".toUpperCase(), style: TextStyles.bodyText),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedGender = "Mujer";
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 8,
+                top: 16,
+                bottom: 16,
+                right: 16,
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: selectedGender == "Mujer"
+                      ? AppColors.backgroundComponentSelected
+                      : AppColors.backgroundComponent,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      Image.asset("assets/images/female.png", height: 100),
+                      Text("Hombre".toUpperCase(), style: TextStyles.bodyText),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
---
+++ lib/components/height_selector.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HeightSelector extends StatefulWidget {
+  const HeightSelector({super.key});
+
+  @override
+  State<HeightSelector> createState() => _HeightSelectorState();
+}
+
+class _HeightSelectorState extends State<HeightSelector> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
