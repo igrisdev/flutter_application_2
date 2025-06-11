9196dea935eed937d59c25eab09e8dc6d39658a2
igrisdev <johanalvarezz237@gmail.com>
2025-06-10 19:06:38 -0500

    Add HeightSelector component and integrate into IMC home screen

--- lib/components/height_selector.dart
+++ lib/components/height_selector.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/core/app_colors.dart';
+import 'package:flutter_application_2/core/text_styles.dart';
 
 class HeightSelector extends StatefulWidget {
   const HeightSelector({super.key});
@@ -8,8 +10,44 @@ class HeightSelector extends StatefulWidget {
 }
 
 class _HeightSelectorState extends State<HeightSelector> {
+  double height = 170; // altura inicial
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppColors.backgroundComponent,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          children: [
+            Text("Altura".toUpperCase(), style: TextStyles.bodyText),
+            Text(
+              "${height.toStringAsFixed(0)} cm",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 38,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Slider(
+              value: height,
+              onChanged: (value) {
+                setState(() {
+                  height = value;
+                });
+              },
+              min: 150,
+              max: 220,
+              divisions: 1,
+              label: "$height",
+              activeColor: AppColors.primary,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/imc_home_screen.dart
+++ lib/screens/imc_home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/components/gender_selector.dart';
+import 'package:flutter_application_2/components/height_selector.dart';
 
 class ImcHomeScreen extends StatefulWidget {
   const ImcHomeScreen({super.key});
@@ -11,6 +12,6 @@ class ImcHomeScreen extends StatefulWidget {
 class _ImcHomeScreenState extends State<ImcHomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(children: [GenderSelector()]);
+    return Column(children: [GenderSelector(), HeightSelector()]);
   }
 }
